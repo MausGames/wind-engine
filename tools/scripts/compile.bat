e6d44fb26008e23b36a3cb931e6458a3cba817a6
Martin Mauersics <revision@maus-games.at>
2021-01-06 17:21:41 +0100

    added missing compile scripts (for Windows)

--- tools/scripts/compile.bat
+++ tools/scripts/compile.bat
@@ -1,5 +1,14 @@
 @ECHO OFF
 
-:: not yet implemented
+SET _EXECUTABLE_=java -jar "%~dp0base\closure-compiler.jar"
+SET _PARAMETERS_=--strict_mode_input --dependency_mode SORT_ONLY --language_in ECMASCRIPT_2015 --language_out ECMASCRIPT_2015 --js '%~dp0..\..\source\**.js' --js '%~1' --js_output_file %~2
 
-PAUSE
+IF "%~3" == "release" (
+    SET _PARAMETERS_=%_PARAMETERS_% --compilation_level ADVANCED --assume_function_wrapper --hide_warnings_for gl-matrix --jscomp_off globalThis
+) ELSE (
+    SET _PARAMETERS_=%_PARAMETERS_% --compilation_level BUNDLE
+)
+
+ECHO %_PARAMETERS_%
+
+CALL %_EXECUTABLE_% %_PARAMETERS_%
